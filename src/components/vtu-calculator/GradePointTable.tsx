e954d6833a6cd90f595b7ead619ea0a4749ed851
ABHISHEK DEVARAJ BHANGI <abhishekdb9999@gmail.com>
2025-05-31 16:33:27 +0000

    in vtu grade point system table remove grade column

--- src/components/vtu-calculator/GradePointTable.tsx
+++ src/components/vtu-calculator/GradePointTable.tsx
@@ -6,6 +6,11 @@ import type { Grade } from "@/types/vtuCalculator";
 import { GRADE_OPTIONS } from "@/types/vtuCalculator";
 
 export function GradePointTable() {
+  // Filter out 'AB' and 'FAIL' grades for display in this table, as their marks range can be ambiguous or 0.
+  // Or, if you want to keep them, ensure marksRange has appropriate values.
+  // For now, let's display all defined grades for completeness of the VTU system.
+  const gradesToDisplay = GRADE_OPTIONS;
+
   return (
     <Card className="mt-6 mb-4 print:hidden">
       <CardHeader>
@@ -16,15 +21,13 @@ export function GradePointTable() {
           <TableHeader>
             <TableRow>
               <TableHead className="text-center">Marks Range</TableHead>
-              <TableHead className="text-center">Grade</TableHead>
               <TableHead className="text-center">Grade Point</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {GRADE_OPTIONS.map((grade) => (
+            {gradesToDisplay.map((grade) => (
               <TableRow key={grade}>
                 <TableCell className="text-center font-medium">{marksRange[grade as Grade]}</TableCell>
-                <TableCell className="text-center font-medium">{grade}</TableCell>
                 <TableCell className="text-center">{gradePoints[grade as Grade]}</TableCell>
               </TableRow>
             ))}
